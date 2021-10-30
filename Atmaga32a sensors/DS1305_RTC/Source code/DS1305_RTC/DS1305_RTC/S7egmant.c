fdfc474573955331fe8cd700298d27db9f7d5831
Ahmed Ayman <31223687+Ahmed0Ayman@users.noreply.github.com>
2021-10-30 04:13:09 +0200

    EXTI fixing bugs

--- Atmaga32a sensors/DS1305_RTC/Source code/DS1305_RTC/DS1305_RTC/S7egmant.c
+++ Atmaga32a sensors/DS1305_RTC/Source code/DS1305_RTC/DS1305_RTC/S7egmant.c
@@ -8,45 +8,63 @@
 #include "S7egmant.h"
 
 
-static Seg7_Handler S7egment_Handler   ;
-
 
 	
-	
-void S7egment_Init(GPIO_TypeDef * Control_S7eg_Port ,GPIO_TypeDef * Seg7_write_Port, uint8_t S7eg_Control_Pin)
+void S7egment_Init(Seg7_Handler *  Segment_Handler)
 {
 		GPIO_InitTypeDef  S7eg_PIN_CONFIG ;
-		S7eg_PIN_CONFIG.pinS = S7eg_Control_Pin ;
-		S7eg_PIN_CONFIG.mode = GPIO_MODE_OUTPUT ;
-		S7eg_PIN_CONFIG.pull = GPIO_NOPULL ;
 
-	S7egment_Handler.Control_S7eg_Port = Control_S7eg_Port ;
-	S7egment_Handler.Seg7_write_Port = Seg7_write_Port ;
-	S7egment_Handler.Num_Of_7Seg = S7eg_Control_Pin;
-	HAL_GPIO_INIT_PIN(S7egment_Handler.Control_S7eg_Port,&S7eg_PIN_CONFIG);
-	S7eg_PIN_CONFIG.pinS = 0x0fu ;
-	HAL_GPIO_INIT_PIN(S7egment_Handler.Seg7_write_Port,&S7eg_PIN_CONFIG);
-	HAL_GPIO_WRITEPIN(S7egment_Handler.Control_S7eg_Port,S7eg_Control_Pin,0);
+	
+	S7eg_PIN_CONFIG.mode = GPIO_MODE_OUTPUT ;
+	S7eg_PIN_CONFIG.pull = GPIO_NOPULL ;
+	/*initialize all control bits  */
+	for (int i=0 ; i < 6 ;i++ )
+	{
+		S7eg_PIN_CONFIG.pinS = Segment_Handler->S7eg_Control[i].Pin_Num ;	
+		HAL_GPIO_INIT_PIN(Segment_Handler->S7eg_Control[i].Port,&S7eg_PIN_CONFIG);	
+	}
+
+
+	/*initialize 7 segment BCD Bits */	
+	for (int i=0 ; i < 4 ;i++ )
+	{
+		S7eg_PIN_CONFIG.pinS = Segment_Handler->S7eg_Bits[i].Pin_Num;
+		HAL_GPIO_INIT_PIN(Segment_Handler->S7eg_Bits[i].Port,&S7eg_PIN_CONFIG);
+	}	
+	
 	
 	
 }
-void S7egment_Write(uint8_t Num_To_S7eg , uint8_t S7eg_Num)
+void S7egment_Write(uint8_t Num_To_S7eg , uint8_t S7eg_Num,Seg7_Handler *  Segment_Handler)
 {
+
+
 	uint8_t High_Num=0,Low_Num=0 ;
 	Low_Num = Num_To_S7eg % 10 ;
+	if (Num_To_S7eg == 0)
+	{
+		
+	}
+	else
+	{
 	Num_To_S7eg /= 10 ;
 	if (Num_To_S7eg > 0){
-		High_Num = Num_To_S7eg % 10 ; 
-	}else{
+		High_Num = Num_To_S7eg % 10 ;
+		}else{
 		High_Num = 0 ;
 	}
+	}
+
+	for(int i =0 ;i<4 ;i++)
+	HAL_GPIO_WRITEPIN(Segment_Handler->S7eg_Bits[i].Port,Segment_Handler->S7eg_Bits[i].Pin_Num,(High_Num>>i)&(0x01));
+	HAL_GPIO_WRITEPIN(Segment_Handler->S7eg_Control[S7eg_Num].Port,Segment_Handler->S7eg_Control[S7eg_Num].Pin_Num,1);
+	_delay_ms(2);
+	HAL_GPIO_WRITEPIN(Segment_Handler->S7eg_Control[S7eg_Num].Port,Segment_Handler->S7eg_Control[S7eg_Num].Pin_Num,0);
+
 	
-	HAL_GPIO_WRITEPIN(S7egment_Handler.Control_S7eg_Port,S7egment_Handler.Num_Of_7Seg , 0);
-	HAL_GPIO_WRITEPORT(S7egment_Handler.Seg7_write_Port,0x0f,High_Num ,0x0f);
-	HAL_GPIO_WRITEPIN(S7egment_Handler.Control_S7eg_Port,S7eg_Num,1);
-	_delay_ms(1);
-	HAL_GPIO_WRITEPIN(S7egment_Handler.Control_S7eg_Port,S7egment_Handler.Num_Of_7Seg , 0);
-	HAL_GPIO_WRITEPORT(S7egment_Handler.Seg7_write_Port,0x0f,Low_Num,0x0f);
-	HAL_GPIO_WRITEPIN(S7egment_Handler.Control_S7eg_Port,S7eg_Num<<1 ,1);
+	for(int i =0 ;i<4 ;i++)
+	HAL_GPIO_WRITEPIN(Segment_Handler->S7eg_Bits[i].Port,Segment_Handler->S7eg_Bits[i].Pin_Num,(Low_Num>>i)&(0x01));
+	HAL_GPIO_WRITEPIN(Segment_Handler->S7eg_Control[S7eg_Num+1].Port,Segment_Handler->S7eg_Control[S7eg_Num+1].Pin_Num,1);
 	_delay_ms(10);	
+	HAL_GPIO_WRITEPIN(Segment_Handler->S7eg_Control[S7eg_Num+1].Port,Segment_Handler->S7eg_Control[S7eg_Num+1].Pin_Num,0);
 }
--- Atmaga32a sensors/DS1305_RTC/Source code/DS1305_RTC/DS1305_RTC/main.c
+++ Atmaga32a sensors/DS1305_RTC/Source code/DS1305_RTC/DS1305_RTC/main.c
@@ -9,31 +9,90 @@
 #include "RTC.h"
 #include "S7egmant.h"
 
+
+#define S7eg_ControlBit0     0x0u
+#define S7eg_ControlBit1     0x1u
+#define S7eg_ControlBit2     0x2u
+#define S7eg_ControlBit3     0x3u
+#define S7eg_ControlBit4     0x4u
+#define S7eg_ControlBit5     0x5u
+
+
+#define S7eg_Bit0			 0x0u
+#define S7eg_Bit1			 0x1u
+#define S7eg_Bit2			 0x2u
+#define S7eg_Bit3			 0x3u
+
 #define F_CPU 16000000 
 #include "util/delay.h"
 
 Time_Typedef  Time ={.Hours =10 ,.Minutes = 20 ,.RTC_Time_Format = RTC_Format_BIN },gettime ={.RTC_Time_Format = RTC_Format_BIN};
 Date_Typedef  Date ={.Day = 1 ,.Year = 20,.month = 11,.RTC_Time_Format = RTC_Format_BIN};
+
+Seg7_Handler S7egHendler = {
+	
+	/* each bit has two attributes Port and Pin number */
+	.S7eg_Bits[S7eg_Bit0].Port = GPIOC ,
+	.S7eg_Bits[S7eg_Bit0].Pin_Num = GPIO_PIN_4 ,
+	.S7eg_Bits[S7eg_Bit1].Port = GPIOC ,
+	.S7eg_Bits[S7eg_Bit1].Pin_Num = GPIO_PIN_5 ,
+	.S7eg_Bits[S7eg_Bit2].Port = GPIOC ,
+	.S7eg_Bits[S7eg_Bit2].Pin_Num = GPIO_PIN_6 ,
+	.S7eg_Bits[S7eg_Bit3].Port = GPIOC ,
+	.S7eg_Bits[S7eg_Bit3].Pin_Num = GPIO_PIN_7 ,
+
+
+	.S7eg_Control[S7eg_ControlBit0].Port = GPIOA ,
+	.S7eg_Control[S7eg_ControlBit0].Pin_Num = GPIO_PIN_0 ,
+	.S7eg_Control[S7eg_ControlBit1].Port = GPIOA ,
+	.S7eg_Control[S7eg_ControlBit1].Pin_Num = GPIO_PIN_1 ,
+	.S7eg_Control[S7eg_ControlBit2].Port = GPIOA ,
+	.S7eg_Control[S7eg_ControlBit2].Pin_Num = GPIO_PIN_2,
+	.S7eg_Control[S7eg_ControlBit3].Port = GPIOA ,
+	.S7eg_Control[S7eg_ControlBit3].Pin_Num = GPIO_PIN_3 ,
+	.S7eg_Control[S7eg_ControlBit4].Port = GPIOA ,
+	.S7eg_Control[S7eg_ControlBit4].Pin_Num = GPIO_PIN_4 ,
+	.S7eg_Control[S7eg_ControlBit5].Port = GPIOA ,
+	.S7eg_Control[S7eg_ControlBit5].Pin_Num = GPIO_PIN_5 ,
+
+	
+	
+};
+
+
 int main(void)
 {
-	uint8_t num =0 ;
 	DS1305_Init();
-
-	S7egment_Init(GPIOD ,GPIOB, 0X3F);
-	S7egment_Write(7 ,GPIO_PIN_0);
+	S7egment_Init(&S7egHendler);
 	RTC_Set_Time(&Time);
 	RTC_Set_Date(&Date);
-    /* Replace with your application code */
-    while (1) 
-    {	
+	
+	//InterrutInt();
+	//EXIT_INT0_CallBack(NULL);
+	
+	
+	//	xBinarySemaSETTask =  xSemaphoreCreateBinary();
+	//	XQueueSETTask      = xQueueCreate(1,sizeof(Set_Message ));
+	
+	
+	
+	//xTaskCreate(MainTask,NULL,400,NULL,1,NULL);//&MainTaskhandler);
+	//xTaskCreate(SETTask,NULL,100,NULL,0,NULL);
+	
+	
+	//vTaskStartScheduler();
+
+
+	/* Replace with your application code */
+	while (1)
+	{
 		RTC_Get_Time(&gettime);
 		
 		
-		S7egment_Write(gettime.Hours ,GPIO_PIN_0);
-		S7egment_Write(gettime.Minutes ,GPIO_PIN_2);
-		S7egment_Write(gettime.Seconds ,GPIO_PIN_4);
-
-		 //RTC_Set_Time(&Time);
-    }
+		S7egment_Write(gettime.Hours  , 0,&S7egHendler);
+		S7egment_Write(gettime.Minutes ,2,&S7egHendler);
+		S7egment_Write(gettime.Seconds ,4,&S7egHendler);
+		
+	}
 }
 
